bfb1f22fe16d699ddbc7bd6aee9b2316a445f216
kavishan <harekavi360@gmail.com>
2025-07-29 09:06:14 +0530

    made to call to the shoeted url

--- app/Http/Controllers/shortnerController.php
+++ app/Http/Controllers/shortnerController.php
@@ -19,33 +19,31 @@ class shortnerController extends Controller
 
         $url = $request->input('url');
 
-        try {
-            $response = Http::timeout(5)->get($url);
-
-            if (!($response->successful())) {
-                return back()->withErrors('error', 'URL is unreachable!');
-            }
-        } catch (\Exception $e) {
-        }
-
+        
         $existing = ShortUrl::where('original_url', $request->url)->first();
+
         if ($existing) return response()->json(['short' => url($existing->short_code)]);
 
-        $code = Str::random(6);
-        while (ShortUrl::where('short_code', $code)->exists()) {
-            $code = Str::random(6);
-        }
+        $code = Str::random(8);
+
+        // while (ShortUrl::where('short_code', $code)->exists()) {
+        //     $code = Str::random(8);
+        // }
+        logger($code);
 
         ShortUrl::create([
             'original_url' => $request->url,
             'short_code' => $code
         ]);
 
-        return response()->json($code);
+        $shorted_url = ShortUrl::where('original_url',$request->url)->first();
+
+        return response()->json(['short' => url($shorted_url->short_code)]);
     }
 
     public function redirect($code)
     {
+        logger('got');
         $short = ShortUrl::where('short_code', $code)->firstOrFail();
         return redirect()->to($short->original_url);
     }
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,7 @@ Route::get('/shortner', function () {
 Route::get('/url-shortner', [shortnerController::class, 'shorten']);
 
 
-// Route::get('')
+
+// Route::get('/127.0.0.1:8000/{code}', [shortnerController::class, 'redirect'])->name('reports-annual')->where('code', '[0-9]+[a-z]+[A-Z]');
+
+Route::get('{shortCode}', [shortnerController::class, 'redirect'])->where('shortCode', '[A-Za-z0-9]+');
